c48b454ecad532ef953f848a0f809ec77422e099
Marko Palinec <marko.palinec@koncar.hr>
2025-03-24 12:04:42 +0100

    end of sec 8

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,17 @@
 import { Grid2 } from "@mui/material";
 import React from "react";
 import ActivityList from "./ActivityList";
+import ActivitzFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>activity filters shal be here</Grid2>
+      <Grid2 size={4}>
+        <ActivitzFilters />
+      </Grid2>
     </Grid2>
   );
 }
